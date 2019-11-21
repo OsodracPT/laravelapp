8eabb9809a584a4fc21b04c43c0177138c11a742
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-11-21 16:37:45 +0000

    Fetching Data With Eloquent

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -16,6 +16,9 @@
             <li>
                 <a class="nav-link" href="/services">Services</a>
             </li>
+            <li>
+                    <a class="nav-link" href="/posts">Blog</a>
+                </li>
             </li>
           </ul>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     </head>
     <body>
         @include('inc.navbar')
-        <div class="container">
+        <div class="container mt-3">
                 @yield('content')
         </div>
     </body>
